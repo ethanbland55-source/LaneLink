c56e68c5bc2a16764b5ad1cecef90d1b14a592ce
ethanbland55-source <ethanbland55@gmail.com>
2026-08-12 18:18:53 +0100

    Clear Repo

--- src/app/robots.ts
+++
@@ -1,9 +0,0 @@
-import type { MetadataRoute } from "next";
-
-export default function robots(): MetadataRoute.Robots {
-  const base = (process.env.NEXT_PUBLIC_SITE_URL ?? "https://carnforthotters.co.uk").replace(/\/$/, "");
-  return {
-    rules: [{ userAgent: "*", allow: "/", disallow: ["/admin", "/api"] }],
-    sitemap: `${base}/sitemap.xml`,
-  };
-}
